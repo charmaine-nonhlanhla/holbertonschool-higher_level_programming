6473c075c8143b50f7b1c1a12fa059de36d2634a
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2024-05-20 07:28:34 +0000

    import

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+import marshal
+import types
+
+def list_module_names(pyc_file):
+    with open(pyc_file, 'rb') as f:
+        f.seek(16)  # Skip the header
+        code = marshal.load(f)
+        return [name for name in dir(types.ModuleType('__main__', code)) if not name.startswith('__')]
+
 if __name__ == "__main__":
-    import hidden_4
-    # interaction in each word of the list
-    for i in dir(hidden_4):
-        if "__" in i:
-            continue
-        else:
-            print(i)
+    names = list_module_names('/tmp/hidden_4.pyc')
+    for name in sorted(names):
+        print(name)
+
