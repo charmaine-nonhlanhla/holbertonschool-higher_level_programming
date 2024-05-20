f4db9ebab259fb8baab52b5d1730fc1b23c04d5f
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2024-05-20 06:58:04 +0000

    hello world

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str1, str2 = "Holberton", "School"
-str1, str2 = str1.capitalize(), str2.capitalize()
-print(f"Welcome to {str1} {str2}!")
-
+number = 3.14159
+print('Float: {:04.2f}'.format(number))
