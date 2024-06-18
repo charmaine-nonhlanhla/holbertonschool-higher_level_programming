f10d70da7ad3e2fc0cd5eca01f96aa3248b0dde5
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2024-06-18 08:04:52 +0000

    Introduction to SQL

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,12 @@
--- Convert the database hbtn_0c_0 to UTF8
+-- Select the database hbtn_0c_0
+USE hbtn_0c_0;
+
+-- Convert the database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the table first_table to UTF8
+-- Convert the table first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the name field in the first_table to UTF8
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the field name in first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
