10888455b07e5cf3b8f0360dc1ec5f10f8a67f3b
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2024-06-19 08:37:55 +0000

    More SQL Queries

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- Script to list all privileges of MySQL users 'user_0d_1' and 'user_0d_2' on localhost
+
+-- Check and list privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Check and list privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
