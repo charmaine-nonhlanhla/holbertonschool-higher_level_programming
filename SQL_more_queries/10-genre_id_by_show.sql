50f48b06bf16a74c1dc0eb885994c3f0aea5fcf2
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2024-06-19 09:47:32 +0000

    more sql queries

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,12 +1,7 @@
 -- Query to list shows with linked genres
 
-SELECT
-    tv_shows.title,
-    tv_show_genres.genre_id
-FROM
-    tv_shows
-JOIN
-    tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY
-    tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 
--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,15 +1,9 @@
 -- Query to list all Comedy shows
 
-SELECT
-    tv_shows.title
-FROM
-    tv_shows
-JOIN
-    tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN
-    tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE
-    tv_genres.name = 'Comedy'
-ORDER BY
-    tv_shows.title ASC;
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy' OR tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC;
 
