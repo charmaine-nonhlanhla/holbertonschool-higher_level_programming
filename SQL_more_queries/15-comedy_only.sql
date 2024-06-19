0c2fb415c84271dd0bcd6a99c665636a2fcc614f
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2024-06-19 10:47:10 +0000

    .

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,8 @@
 -- Query to list all Comedy shows
 
-SELECT tv_shows.title
+SELECT CONCAT('title', '\n', GROUP_CONCAT(title ORDER BY title SEPARATOR '\n'))
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy' OR tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy';
 
